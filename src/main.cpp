c91c3767301261875cdeaecfc54e757e97d7c8be
joe <joe@ubuntu.myguest.virtualbox.org>
2024-06-01 23:40:03 +0100

    fixed compiling issues now has basic window and cleaned up directories

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 
-#include "imgui.h"
-#include "imgui_impl_sdl2.h"
-#include "imgui_impl_opengl3.h"
+#include "../lib/imgui/imgui.h"
+#include "../lib/imgui/imgui_impl_sdl2.h"
+#include "../lib/imgui/imgui_impl_opengl3.h"
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <stdio.h>
 #include <GL/gl.h>
+
+
 
 int main()
 {
@@ -14,7 +16,7 @@ int main()
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
  
-    const char* glsl_version = "#version 150";
+    const char* glsl_version = "#version 130";
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -30,7 +32,7 @@ int main()
 
     if(!window)
     {
-        std::cout << "Failed to create window\n";
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         return -1;
     }
 
@@ -42,7 +44,8 @@ int main()
 
     }
     bool gQuit = false;
-    SDL_Event event;
+   
+  
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1);
     SDL_UpdateWindowSurface(window);
@@ -60,7 +63,11 @@ int main()
 
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
-   
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    SDL_Event event;
     while(!gQuit)
     {
         while(SDL_PollEvent(&event))
@@ -69,14 +76,44 @@ int main()
             {
                 case SDL_QUIT:
                     gQuit = true;
-                    break;
+                    break;   
+
             }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                gQuit = true;
+            
+
         }
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
+
+        // ImGuiViewport* viewport = ImGui::GetMainViewport();
+        // ImGui::SetNextWindowPos(viewport->GetWorkPos());
+        // ImGui::SetNextWindowSize(viewport->GetWorkSize());
+        // ImGui::SetNextWindowViewport(viewport->ID);
+
         ImGui::NewFrame();
+        ImGui::Begin("test");
+        ImGui::End();
 
 
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+            SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+        }
+
+        SDL_GL_SwapWindow(window);
 
     }
     ImGui_ImplOpenGL3_Shutdown();
